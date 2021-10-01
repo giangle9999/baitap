6efb9008979d0fa687c557f118b0cba4d9c11228
min <min@DESKTOP-T2NAVI1>
2021-10-01 14:03:49 +0700

    Acc

---
+++ BT2/Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using BT2.Models;
+
+namespace BT2.Controllers
+{
+    public class AccountController : Controller
+    {
+        Encrytion encry = new Encrytion();
+        LTQLEntities db = new LTQLEntities();
+
+        // GET: Account
+       [HttpGet]
+       public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        public ActionResult Login(Account Acc)
+        {
+            if (ModelState.IsValid)
+            {
+                string encrytionpass = encry.PasswordEncrytion(Acc.Password);
+                var model = db.Accounts.Where(m => m.UserName == Acc.UserName && m.Password == encrytionpass).Tolist().Count();
+                if (model == 1)
+                {
+                    FormsAuthentication.SetAuthCookie(Acc.UserName, true);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Thông tin đăng nhập không chính xác");
+
+                }    
+                   
+
+            }
+            return View(Acc);
+            
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+
+        }
+            
+            
+    }
+
+}
